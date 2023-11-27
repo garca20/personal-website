77ca928505a2c0ef1fec8e0cb60ae21850dc142f
garca20 <anfemma01@gmail.com>
2023-11-26 23:17:34 -0500

    Updating Before Products Fix

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function redirectTo(redirect) {
+    window.location.replace(redirect);
+    window.location.reload;
+}
+
+function chequearCupon() {
+    const cupones = ["CODIGO20", "CODIGO40", "CODIGO60"];
+}
