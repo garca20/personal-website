99cc2409027559fd64e026eb25d595d84e09364c
garca20 <anfemma01@gmail.com>
2023-11-27 23:10:40 -0500

    Fixed placeholders and linking

--- js/index.js
+++ js/index.js
@@ -2,7 +2,3 @@ function redirectTo(redirect) {
     window.location.replace(redirect);
     window.location.reload;
 }
-
-function chequearCupon() {
-    const cupones = ["CODIGO20", "CODIGO40", "CODIGO60"];
-}
---
+++ js/manejoDeCupones.js
@@ -0,0 +1,37 @@
+const listaCupones = ["DISC-2023", "30BY-EBYE", "CINC-UENT"]
+const listaCuponesEfectos = [0.75, 0.7, 0.5]
+
+
+function validarNumero(precio) {
+    let dinero = document.getElementById("numeroA").value
+    let esNumero = !(isNaN(dinero) || dinero.length == 0)
+    if (esNumero) {
+        if (dinero<precio) {
+            document.getElementById("warning_text_1").innerText = "No hay dinero suficiente"
+        }
+        else {
+            var dineroRestante = (parseInt(dinero)-parseInt(precio))
+            document.getElementById("warning_text_1").innerText = "Validado, dinero restante: $"+dineroRestante
+            document.getElementById("cupon").value = ""
+        }
+    }
+    else {
+        document.getElementById("warning_text_1").innerText = "No es un número válido"
+    }
+}
+
+function validarCupon(precio) {
+    let cupon = document.getElementById("cupon").value
+    for (let posicion=0; posicion<listaCupones.length; posicion++) {
+        if (listaCupones[posicion]==cupon) {
+            let dinero = document.getElementById("numeroA").value
+            var dineroRestante = (parseInt(dinero)-(parseInt(precio)*listaCuponesEfectos[posicion]))
+            document.getElementById("warning_text_1").innerText = "Cupón aplicado, dinero restante: $"+dineroRestante
+        }
+    }
+}
+
+function alertar() {
+    alert("Producto no encontrado :(")
+    document.getElementById("search").value = ""
+}
